b29be6f7443c3f00dc2a8c121611a0f2ca508e93
karan rawat <rawatkaran181@gmail.com>
2021-08-20 13:35:22 +0530

    refector code and add static method

--- Server/database/user/index.js
+++ Server/database/user/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-
-
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 //Schema
 const userSchema = new mongoose.Schema({
 
@@ -15,6 +15,48 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
   }
 );
+
+
+
+//
+
+
+userSchema.methods.generateJwtToken = function(){
+
+  return  jwt.sign({user: this._id.toString() },"ZomatoApp");
+}
+
+
+//static functoin
+userSchema.statics.findByEmailAndPhone = async({email,phoneNumber})=>{
+  const checkUserEmail = await UserModel.findOne({ email });
+  const checkUserPhone = await UserModel.findOne({ phoneNumber });
+
+  if(checkUserEmail || checkUserPhone){
+  throw new Error("user ALready Exist");
+}
+
+return false;
+};
+
+
+userSchema.pre("save", function (next){
+     
+      const user = this;
+      
+      if(!user.isModified("password")) return next();
+
+      bcrypt.genSalt(8, (error,salt)=>{
+
+        if(error) next(error);
+    
+        bcrypt.hash(user.password, salt, (error,hash)=>{
+          if(error) return next(error)
+          user.password = hash;
+          return next();
+        });
+      });
+});
 
 //model
 export const UserModel = mongoose.model("Users", userSchema);
